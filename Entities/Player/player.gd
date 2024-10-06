7b90e50a6a0b25302f25e9ba6246c9c4f6cec1b0
Peazy <email@domain>
2024-10-06 18:27:04 -0500

    Move health to Player.tscn

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
+@onready var health: Node = $Health
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		print(health.health)
 
 func _physics_process(delta: float) -> void:
-
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
