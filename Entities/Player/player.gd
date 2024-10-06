e86811a926592c249c797163f4312ef5e5511817
HalpinLiam <92271363+HalpinLiam@users.noreply.github.com>
2024-10-06 18:42:52 -0500

    Revert "reverted change"
    This reverts commit 1138cbc97f8718837ea9fb5a83df9d3baffcac45.

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-
-const SPEED = 150.0
+signal tookDamage(damage : int)
+signal healed(health : int)
+signal died()
 
 @onready var health: Node = $Health
+
+const SPEED = 150.0
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		print(health.health)
+
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -20,3 +24,15 @@ func _physics_process(delta: float) -> void:
 	#	velocity = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_health_died() -> void:
+	died.emit()
+
+
+func _on_health_healed(health: int) -> void:
+	healed.emit(health)
+
+
+func _on_health_took_damage(damage: int) -> void:
+	tookDamage.emit(damage)
