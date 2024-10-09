06671cd7024b2de4631359eba7a5f9fd89fa9c70
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-10-08 23:38:58 -0500

    Added score graphics

--- Environment/score.gd
+++ Environment/score.gd
@@ -3,4 +3,4 @@ extends RichTextLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.text = "Score: " + str(Level.score)
+	self.text = "[center]" + str(Level.score) + "[/center]"
