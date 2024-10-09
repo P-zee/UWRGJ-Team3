edfdf320061540d84574ad1d23d896cea103e392
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-10-08 22:47:28 -0500

    Fixed typo

--- Environment/player_health_bar.gd
+++ Environment/player_health_bar.gd
@@ -4,7 +4,7 @@ extends TextureProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.max_value = player.health.maxHealthp
+	self.max_value = player.health.maxHealth
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
