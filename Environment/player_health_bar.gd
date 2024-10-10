9e54c4e685da65b3086f67ef0d3829dd1e6952ea
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-10-09 19:57:36 -0500

    Health bars have smoother changing

--- Environment/player_health_bar.gd
+++ Environment/player_health_bar.gd
@@ -1,18 +1,21 @@
 extends TextureProgressBar
 
 @onready var player: Node = %Player
-
 @export var offset: Vector2
+@export var smoothing: float
+
+var maxHealth: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.max_value = player.health.maxHealth
+	maxHealth = player.health.maxHealth
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var center = Vector2(size.x/2, size.y/2)
-	
 	self.position = player.position - center + offset
 	
-	self.value = player.health.health
+	var actualHeatlh = (player.health.health / maxHealth) * 100
+	
+	self.value = lerp(self.value, actualHeatlh, smoothing)
--- Environment/queen_health_bar.gd
+++ Environment/queen_health_bar.gd
@@ -4,15 +4,19 @@ extends TextureProgressBar
 
 @export var offset: Vector2
 
+@export var smoothing: float
+var maxHealth: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.max_value = queen.health.maxHealth
+	maxHealth = queen.health.maxHealth
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var center = Vector2(size.x/2, size.y/2)
-	
 	self.position = queen.position - center + offset
 	
-	self.value = queen.health.health
+	var actualHeatlh = (queen.health.health / maxHealth) * 100
+	
+	self.value = lerp(self.value, actualHeatlh, smoothing)
