f3d7e294316bf8b81dcd7ba49ab12944a02e7ba9
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-10-09 19:22:57 -0500

    Healthbar follows queen

--- Environment/queen_health_bar.gd
+++ Environment/queen_health_bar.gd
@@ -2,6 +2,8 @@ extends TextureProgressBar
 
 @onready var queen: Node = %Queen
 
+@export var offset: Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.max_value = queen.health.maxHealth
@@ -9,4 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var center = Vector2(size.x/2, size.y/2)
+	
+	self.position = queen.position - center + offset
+	
 	self.value = queen.health.health
