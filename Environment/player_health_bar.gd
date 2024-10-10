ce009316826693b4d40cc9d9a63d9b1f8c5a2521
Tetratoric <114266944+tetratoric@users.noreply.github.com>
2024-10-09 19:04:08 -0500

    Player health bar follows player

--- Environment/player_health_bar.gd
+++ Environment/player_health_bar.gd
@@ -1,12 +1,13 @@
 extends TextureProgressBar
 
-@onready var player: Node = %Player
+var parent = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.max_value = player.health.maxHealth
+	parent = get_parent()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.value = player.health.health
+	self.value = parent.health.health
+	self.max_value = parent.health.maxHealth
