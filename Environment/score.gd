aa5689ac1950678acdfb2837dc1e0b9f15ef33e8
HalpinLiam <92271363+HalpinLiam@users.noreply.github.com>
2024-10-10 20:08:56 -0500

    Linked Score

--- Environment/score.gd
+++ Environment/score.gd
@@ -1,6 +1,7 @@
 extends RichTextLabel
 
+@onready var level: Node2D = $"../.."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.text = "[center]" + str(Level.score) + "[/center]"
+	self.text = "[center]" + str(level.score) + "[/center]"
